edde28552472960183f5099e44fac6d895ca0646
YoitsVansh <vansh.199301021@muj.manipal.edu>
2023-01-12 00:00:50 +0530

    Added React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, {FC} from 'react';
 import BottomPane from './Components/BottomPane';
 import TopPane from './Components/TopPane';
-
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 const App: FC = () => {
+  const client = new QueryClient();
   return (
-    <div className="w-screen bg-slate-50 h-full flex flex-col">
-      <h1 className="p-4 py-2 font-semibold text-3xl text-neutral-800">
-        {' '}
-        Company Settings
-      </h1>
-      <TopPane />
-      <BottomPane />
-    </div>
+    <QueryClientProvider client={client}>
+      <div className="w-screen bg-slate-50 h-full flex flex-col">
+        <h1 className="p-4 py-2 font-semibold text-3xl text-neutral-800">
+          {' '}
+          Company Settings
+        </h1>
+        <TopPane />
+        <BottomPane />
+      </div>
+    </QueryClientProvider>
   );
 };
 
--- src/Components/BottomPane.tsx
+++ src/Components/BottomPane.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import MainTable from './MainTable';
+import {useQuery} from '@tanstack/react-query';
+import axios from 'axios';
 
 function BottomPane() {
+  const {data} = useQuery(['users'], () => {
+    return axios
+      .get('https://mocki.io/v1/36a6f4d3-303a-4464-aa7e-d54939b08fae')
+      .then(res => res.data);
+  });
   return (
     <div className="p-4 grow ">
-      <MainTable />
+      {data !== undefined && <MainTable data={data} />}
     </div>
   );
 }
